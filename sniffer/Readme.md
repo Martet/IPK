ed0a7f0e77d4ddadab262b6d354b074636ee2df4
Martet <martin@zmitko.cz>
2022-04-24 23:54:49 +0200

    Done sniffer

--- sniffer/Readme.md
+++ sniffer/Readme.md
@@ -4,38 +4,56 @@ A packet sniffer written in c++ using the libpcap library.
 
 ## Compilation
 
-Only standard unix libraries are used and a Makefile is included, so the only prerequisite is gcc. Compile running the `make` command, a `hinfosvc` binary file will be created.
+Only standard unix libraries and libpcap are used and a Makefile is included. Compile running the `make` command, a `ipk-sniffer` binary file will be created.
 
 ## Usage
 
-Start the `hinfosvc` binary with the desired port as an argument and leave it running on the background. 
+Start the `ipk-sniffer` with the desired arguments and captured packets will be printed out to stdout.
 
 ```
-./hinfosvc <PORT>
+./ipk-sniffer [-i interface | --interface interface] {-p ­­port} {[--tcp|-t] [--udp|-u] [--arp] [--icmp] } {-n num}
 ```
 
-There are 3 endpoints, to which GET requests can be sent and the corresponding information will be returned as a plaintext response. Those endpoints are:
+Where:
 
-* `/hostname` - gets the hostname of the server
-* `/cpu-name` - gets the name of the cpu of the server
-* `/load` - gets the current cpu load of the server
+* `interface` - the name of the desired interface to sniff on, if not enter, output available interfaces
+* `port` - desired port to sniff on (if not entered, sniff on all ports)
+* `num` - number of captured packets before program ends
+* `tcp`, `udp`, `arp`, `icmp` - capture only this type of packet
 
 ## Examples:
 
 ```
-$ curl localhost:12345/hostname
-merlin.fit.vutbr.cz
-
-$ curl localhost:12345/cpu-name
-Intel(R) Xeon(R) Silver 4214R CPU @ 2.40GHz
-
-$ curl localhost:12345/load
-12%
+./ipk-sniffer -i eth0 -p 23 --tcp -n 2
+./ipk-sniffer -i eth0 --udp
+./ipk-sniffer -i eth0 -n 10      
+./ipk-sniffer -i eth0 -p 22 --tcp
 ```
 
-## Author and additional information
+## Example output:
+
+```
+$./ipk-sniffer -i eth0
+
+timestamp: 2021-03-19T18:42:52+01:00
+src MAC: 00:1c:2e:92:03:80
+dst MAC: 00:1b:3f:56:8a:00
+frame length: 512 bytes
+src IP: 147.229.13.223
+dst IP: 10.10.10.56
+src port: 4093
+dst port: 80
+
+0x0000:  00 19 d1 f7 be e5 00 04  96 1d 34 20 08 00 45 00  ........ ..4 ..
+0x0010:  05 a0 52 5b 40 00 36 06  5b db d9 43 16 8c 93 e5  ..R[@.6. [..C....
+0x0020:  0d 6d 00 50 0d fb 3d cd  0a ed 41 d1 a4 ff 50 18  .m.P..=. ..A...P.
+0x0030:  19 20 c7 cd 00 00 99 17  f1 60 7a bc 1f 97 2e b7  . ...... .`z.....
+0x0040:  a1 18 f4 0b 5a ff 5f ac 07 71 a8 ac 54 67 3b 39  ....Z._. .q..Tg;9
+0x0050:  4e 31 c5 5c 5f b5 37 ed  bd 66 ee ea b1 2b 0c 26  N1.\_.7. .f...+.&
+0x0060:  98 9d b8 c8 00 80 0c 57  61 87 b0 cd 08 80 00 a1  .......W a.......
+```
+
+## Author
 
 Martin Zmitko (xzmitko@stud.fit.vutbr.cz)
-
-Cpu load is calculated with a calculation taken from https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
 
---
+++ sniffer/ipk-sniffer.cpp
@@ -0,0 +1,154 @@
+#include "pcap.h"
+#include <iostream>
+#include <getopt.h>
+#include <string>
+#include <string.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <net/if_arp.h>
+
+void error(std::string message){
+    std::cerr << message << "\n";
+    exit(1);
+}
+
+void handler(u_char *args, const struct pcap_pkthdr *pkthdr, const u_char *packet){
+    char buf[100];
+    strftime(buf, 99, "%FT%T%z", localtime(&pkthdr->ts.tv_sec));
+    std::cout << "timestamp: " << buf << "\n";
+    (void)args;
+
+    struct ether_header *header = (struct ether_header *) packet;
+    std::cout << "src MAC: " << ether_ntoa((const struct ether_addr *) header->ether_shost) << "\n";
+    std::cout << "dst MAC: " << ether_ntoa((const struct ether_addr *) header->ether_dhost) << "\n";
+    std::cout << "frame length: " << pkthdr->len << " bytes\n";
+
+    u_short type = ntohs(header->ether_type);
+    if(type == ETHERTYPE_IP){
+        const struct ip *ip = (const struct ip *) (packet + sizeof(struct ether_header));
+        std::cout << "src IP: " << inet_ntoa(ip->ip_src) << "\n";
+        std::cout << "dst IP: " << inet_ntoa(ip->ip_dst) << "\n";
+
+        int offset = sizeof(struct ether_header);
+        if(ip->ip_p == IPPROTO_TCP){
+            struct tcphdr *tcp = (struct tcphdr *)(packet + sizeof(struct ether_header) + (ip->ip_hl & 0x0F) * 4);
+            std::cout << "src port: " << tcp->th_sport << "\n";
+            std::cout << "dst port: " << tcp->th_dport << "\n";
+            offset += (ip->ip_hl & 0x0F) * 4 + ((tcp->th_off & 0xF0) >> 4);
+        }
+        else if(ip->ip_p == IPPROTO_UDP){
+            struct udphdr *udp = (struct udphdr *)(packet + sizeof(struct ether_header) + (ip->ip_hl & 0x0F) * 4);
+            std::cout << "src port: " << udp->uh_sport << "\n";
+            std::cout << "dst port: " << udp->uh_dport << "\n";
+            offset += (ip->ip_hl & 0x0F) * 4 + sizeof(struct udphdr);
+        }
+        else
+            return;
+        
+        const u_char *payload = packet + offset;
+        for(unsigned int i = 0; i < pkthdr->len; i += 16){
+            printf("0x%04hhx: ", i);
+            for(unsigned int j = 0; j < 16; j++){
+                printf("%02hhx ", payload[i + j]);
+            }
+            for(unsigned int j = 0; j < 16; j++){
+                printf("%c ", payload[i + j] >= 32 && payload[i + j] <= 127 ? payload[i + j] : '.');
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main(int argc, char *argv[]){
+    int flag, filter = 0;
+    struct option options[] = {
+        {"interface", required_argument, NULL, 'i'}, 
+        {"tcp", no_argument, &flag, 't'},
+        {"udp", no_argument, &flag, 'u'},
+        {"arp", no_argument, &flag, 'a'},
+        {"icmp", no_argument, &flag, 'c'},
+        {0, 0, 0, 0}};
+    int c, port = 0, n = 1;
+    std::string interface;
+    while((c = getopt_long(argc, argv, "i:tuacp:n:", options, NULL)) != -1){
+        switch (c){
+            case 'i':
+                interface = std::string(optarg);
+                break;
+            case 0:
+                if(filter && filter != flag)
+                    error("Argument parsing error");
+                filter = flag;
+                break;
+            case 't': case 'u':
+                if(filter && filter != c)
+                    error("Argument parsing error");
+                filter = c;
+                break;
+            case 'n': case 'p':
+                try{
+                    if(c == 'n')
+                        n = std::stoi(optarg, nullptr, 10);
+                    else
+                        port = std::stoi(optarg, nullptr, 10);
+                } catch(...){
+                    error("Argument parsing error");
+                }
+                break;
+            case '?': default:
+                error("Argument parsing error");
+        }
+    }
+
+    if(interface == ""){
+        pcap_if_t *devs;
+        if(pcap_findalldevs(&devs, NULL) == PCAP_ERROR)
+            error("Error getting interfaces");
+        pcap_if_t *dev = devs;
+        while(dev){
+            std::cout << dev->name << "\n";
+            dev = dev->next;
+        }
+        pcap_freealldevs(devs);
+        return 0;
+    }
+
+    char errbuf[PCAP_ERRBUF_SIZE];
+    bpf_u_int32 ip, mask;
+    if(pcap_lookupnet(interface.c_str(), &ip, &mask, errbuf) == PCAP_ERROR)
+        error("Error looking up interface: " + std::string(errbuf));
+
+    pcap_t *pcap;
+    if((pcap = pcap_open_live(interface.c_str(), BUFSIZ, 1, -1, errbuf)) == NULL)
+        error("Error opening interface for sniffing: " + std::string(errbuf));
+
+    std::string filter_str;
+    if(filter)
+        switch(filter){
+            case 't':
+                filter_str = "tcp ";
+                break;
+            case 'u':
+                filter_str = "udp ";
+                break;
+            case 'a':
+                filter_str = "arp ";
+                break;
+            case 'c':
+                filter_str = "icmp ";
+                break;
+        }
+    if(port)
+        filter_str += "port " + std::to_string(port);
+
+    struct bpf_program program;
+    if(pcap_compile(pcap, &program, filter_str.c_str(), 0, ip) == PCAP_ERROR)
+        error("Error compiling filters");
+    if(pcap_setfilter(pcap, &program) == PCAP_ERROR)
+        error("Error applying filters");
+    
+    return pcap_loop(pcap, n, handler, NULL);
+}
+
